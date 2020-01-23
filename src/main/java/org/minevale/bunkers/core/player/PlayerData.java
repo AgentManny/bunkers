26df8875e038e7dfd73071f2619457bd1293f44c
manny <itsreallymanny@gmail.com>
2020-01-23 18:38:05 +0000

    Load mongo player data

--- src/main/java/org/minevale/bunkers/core/player/PlayerData.java
+++ src/main/java/org/minevale/bunkers/core/player/PlayerData.java
@@ -6,6 +6,8 @@ import lombok.Setter;
 import org.bson.Document;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
+import org.minevale.bunkers.core.BunkersCore;
+import org.minevale.bunkers.core.player.inventory.PlayerInventoryData;
 
 import java.util.UUID;
 
@@ -17,21 +19,33 @@ public class PlayerData {
     private final UUID uuid;
     private final String username;
 
-    private int coins;
-    private int nuggets;
-    private int bars;
+    private PlayerInventoryData inventoryData;
+
+    private int coins = 0;
+    private int nuggets = 0;
+    private int bars = 0;
 
     private boolean needsSaving = false;
 
     public void update(Document document) {
         if (document == null) {
+            Player player = getPlayer();
+            if (player != null) {
+                this.inventoryData = new PlayerInventoryData(player);
+            }
+            save(); // Saves to disk
             return;
         }
 
-        Document balanceData = document.get("Document", Document.class);
+        Document balanceData = document.get("balance", Document.class);
         this.coins = balanceData.getInteger("coins", 0);
         this.nuggets = balanceData.getInteger("nuggets", 0);
         this.bars = balanceData.getInteger("bars", 0);
+    }
+
+    public void save() {
+        needsSaving = false;
+        BunkersCore.getInstance().getPlayerDataManager().save(this);
     }
 
     public Document toDocument() {
---
+++ src/main/java/org/minevale/bunkers/core/player/inventory/PlayerInventoryData.java
@@ -0,0 +1,68 @@
+package org.minevale.bunkers.core.player.inventory;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.bson.Document;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.potion.PotionEffect;
+import org.minevale.bunkers.core.BunkersCore;
+
+@Getter
+@Setter
+public class PlayerInventoryData {
+
+    private final PotionEffect[] effects;
+
+    private final int health;
+    private final int hunger;
+
+    private final ItemStack[] armor;
+    private final ItemStack[] contents;
+
+    public PlayerInventoryData(Player player) {
+        int i;
+        ItemStack stack;
+        this.contents = player.getInventory().getContents();
+        for (i = 0; i < this.contents.length; ++i) {
+            stack = this.contents[i];
+            if (stack != null) continue;
+            this.contents[i] = new ItemStack(Material.AIR, 0, (short) 0);
+        }
+        this.armor = player.getInventory().getArmorContents();
+        for (i = 0; i < this.armor.length; ++i) {
+            stack = this.armor[i];
+            if (stack != null) continue;
+            this.armor[i] = new ItemStack(Material.AIR, 0, (short) 0);
+        }
+        this.effects = player.getActivePotionEffects().toArray(new PotionEffect[0]);
+        this.health = (int) player.getHealth();
+        this.hunger = player.getFoodLevel();
+    }
+
+    public void apply(Player player) {
+        player.getInventory().setContents(this.contents);
+        player.getInventory().setArmorContents(this.armor);
+        for (PotionEffect effect : player.getActivePotionEffects()) {
+            player.removePotionEffect(effect.getType());
+        }
+        for (PotionEffect effect : this.effects) {
+            player.addPotionEffect(effect);
+        }
+    }
+
+    public static String serialize(Player player) {
+        return BunkersCore.GSON.toJson(new PlayerInventoryData(player));
+    }
+
+    public static PlayerInventoryData deserialize(String json) {
+        return BunkersCore.GSON.fromJson(json, PlayerInventoryData.class);
+    }
+
+    public static Document getAsDocument(Player player) {
+        return Document.parse(PlayerInventoryData.serialize(player));
+    }
+
+
+}
