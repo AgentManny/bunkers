bcfbab3b4ab479c53acabd8d2ce9bf8d0ab22562
manny <itsreallymanny@gmail.com>
2020-01-23 17:23:08 +0000

    Save the players balance data

--- src/main/java/org/minevale/bunkers/core/player/PlayerData.java
+++ src/main/java/org/minevale/bunkers/core/player/PlayerData.java
@@ -17,6 +17,10 @@ public class PlayerData {
     private final UUID uuid;
     private final String username;
 
+    private int coins;
+    private int nuggets;
+    private int bars;
+
     private boolean needsSaving = false;
 
     public void update(Document document) {
@@ -24,11 +28,20 @@ public class PlayerData {
             return;
         }
 
+        Document balanceData = document.get("Document", Document.class);
+        this.coins = balanceData.getInteger("coins", 0);
+        this.nuggets = balanceData.getInteger("nuggets", 0);
+        this.bars = balanceData.getInteger("bars", 0);
     }
 
     public Document toDocument() {
+        Document balanceData = new Document("coins", coins)
+                .append("nuggets", nuggets)
+                .append("bars", bars);
+
         return new Document("uuid", uuid.toString())
-                .append("username", username);
+                .append("username", username)
+                .append("balance", balanceData);
     }
 
     public Player getPlayer() {
